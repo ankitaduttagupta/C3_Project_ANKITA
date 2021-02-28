6e91f1500530942725eed59bfad4d1d09287b731
Ankita Dutta Gupta <53208816+ankitaduttagupta@users.noreply.github.com>
2021-02-28 17:40:38 +0530

    Add files via upload

--- Restaurant_project -Student version/src/test/java/RestaurantTest.java
+++ Restaurant_project -Student version/src/test/java/RestaurantTest.java
@@ -1,40 +1,40 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class RestaurantTest {
     Restaurant restaurant;
-    RestaurantService service = new RestaurantService();
+
 
     //REFACTORING ALL THE REPEATED LINES OF CODE
     @BeforeEach
     public void setup(){
-        restaurant= service.addRestaurant("foodhub","thane",LocalTime.parse("10:30:00"),LocalTime.parse("23:50:00"));
+        restaurant= new Restaurant("foodhub","thane",LocalTime.parse("10:30:00"),LocalTime.parse("23:50:00"));
         restaurant.addToMenu("Sweet corn soup",120);
         restaurant.addToMenu("Vegetable lasagne", 250);
 
     }
- @Test
+    @Test
     public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time(){
-        //WRITE UNIT TEST CASE HERE
-     LocalTime currentTime= LocalTime.parse("22:50:00") ;
-     Assertions.assertTrue(currentTime.isAfter(restaurant.openingTime)&& currentTime.isBefore(restaurant.closingTime));
+        LocalTime currentTime= LocalTime.parse("22:50:00") ;
+        Assertions.assertEquals(restaurant.isRestaurantOpen(),currentTime.isAfter(restaurant.openingTime)&& currentTime.isBefore(restaurant.closingTime));
 
- }
+
+    }
 
     @Test
     public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
         //WRITE UNIT TEST CASE HERE
-        LocalTime currentTime= LocalTime.parse("22:50:00") ;
-        Assertions.assertFalse(currentTime.isBefore(restaurant.openingTime)&& currentTime.isAfter(restaurant.closingTime));
-
-
+        LocalTime currentTime= LocalTime.parse("06:00:00") ;
+        Assertions.assertNotEquals(restaurant.isRestaurantOpen(),currentTime.isBefore(restaurant.openingTime)&& currentTime.isAfter(restaurant.closingTime));
     }
 
     //<<<<<<<<<<<<<<<<<<<<<<<<<OPEN/CLOSED>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
